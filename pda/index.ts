b6d91efd91707741ea84295069ffa792046cb4ec
arnabnandikgp <arnabnandi2002@gmail.com>
2025-06-17 21:42:40 +0530

    minor changes
    Signed-off-by: arnabnandikgp <arnabnandi2002@gmail.com>

--- pda/index.ts
+++ pda/index.ts
@@ -15,21 +15,3 @@ for (let bump = 255; bump >= 0; bump--) {
     console.log("bump " + bump + ": " + error);
   }
 }
-
-
-// import { PublicKey } from "@solana/web3.js";
-
-const programAddress = new PublicKey("11111111111111111111111111111111");
-
-const optionalSeedString = "helloWorld";
-// const optionalSeedAddress = new PublicKey(
-//   "B9Lf9z5BfNPT4d5KMeaBFx8x1G4CULZYR1jA2kmxRDka"
-// );
-const seeds = [Buffer.from(optionalSeedString)];
-const [pda, bump] = await PublicKey.findProgramAddrcessSync(
-  seeds,
-  programAddress
-);
-
-console.log(`the only and only one PDA: ${pda}`);
-console.log(`Bump: ${bump}`);   
